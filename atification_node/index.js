db5f71f20e39b72c9b42ce433fb0d9ff1eaf5f0c
Yeongbeom Song <gnyf0915@gmail.com>
2023-03-03 16:19:53 +0900

    Feat: Separating the Notice List
    공지사항 리스트 분리 구현
    - `importantNoticeList`와 `generalNoticeList`리스트로 분리
    - 리스트 확인을 위한 디버깅 코드 추가

--- atification_node/index.js
+++ atification_node/index.js
@@ -1,34 +1,51 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const getHTML = async () => {
+const getNoticeList = async () => {
   try {
     const html = await axios.get("https://www.ajou.ac.kr/kr/ajou/notice.do?mode=list&&articleLimit=10&article.offset=0");
     const $ = cheerio.load(html.data);
 
-    let noticeList = [];
-    const bodyList = $("tr");
+    let importantNoticeList = [];
+    let generalNoticeList = [];
 
-    bodyList.map((i, element) => {
+    let importantNoticeIndex = 0;
+    let generalNoticeIndex = 0;
+
+    const bodyList = $("tbody > tr");
+
+    bodyList.map((index, element) => {
         let cellList = $(element).find("td").text().replace(/[\t\r\n\v\f]+/g, "|").split("|");
         let cellLink = $(element).find("td").find("a").attr("href");
 
-        if (cellList[1] !== "공지") {
-            noticeList[i] = {
-                number: cellList[1],
-                cartegory: cellList[2],
-                title: cellList[3],
-                writer: cellList[8],
-                date: cellList[9],
-                link: `https://www.ajou.ac.kr/kr/ajou/notice.do${cellLink}`
-            };
+        if (cellList[1] === "공지") {
+          importantNoticeList[importantNoticeIndex++] = {
+            number: cellList[1],
+            category: cellList[2],
+            title: cellList[6],
+            writer: cellList[16],
+            date: cellList[17],
+            link: `https://www.ajou.ac.kr/kr/ajou/notice.do${cellLink}`
+          };
+        } else {
+          generalNoticeList[generalNoticeIndex++] = {
+            number: cellList[1],
+            category: cellList[2],
+            title: cellList[3],
+            writer: cellList[8],
+            date: cellList[9],
+            link: `https://www.ajou.ac.kr/kr/ajou/notice.do${cellLink}`
+          };
+
+          generalNoticeIndex++;
         }
     });
 
-    console.log("bodyList : ", noticeList);
+    console.log("Important Notice: ", importantNoticeList);
+    console.log("General Notice: ", generalNoticeList);
   } catch (error) {
     console.error(error);
   }
 };
 
-getHTML();
+await getNoticeList();
